5c6c9d84762bb90326e9f6590ce2106156327f2d
Hardeep Singh <703625@student.tor.loc>
2019-03-28 17:11:54 -0400

    1 COMMIT

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Download();
+            Console.ReadLine();
+        }
+        static void Download()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Download");
+        }
+    }
+}
